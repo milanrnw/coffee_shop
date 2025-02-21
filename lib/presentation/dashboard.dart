2aaf1d74eea18eef916c852c14759ea94d6b4394
Milan <8134milanrnw@gmail.com>
2025-02-21 15:39:30 +0530

    added onboarding screens

---
+++ lib/presentation/dashboard.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatelessWidget {
+  const Dashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Text("Hello!"),
+      ),
+    );
+  }
+}
