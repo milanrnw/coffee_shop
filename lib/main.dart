9d9e2e3c22cb2b1505d948978186015c6610057a
Milan <8134milanrnw@gmail.com>
2025-12-11 21:15:46 +0530

    fixed app's orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:coffee_shop/presentation/splash/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   final prefs = await SharedPreferences.getInstance();
   final bool seenOnboarding = prefs.getBool('seenOnboarding') ?? false;
   runApp(MyApp(showOnboarding: !seenOnboarding));
